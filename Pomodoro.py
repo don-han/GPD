60f51328a4087eabc6630c8becf462a8495d4f95
Don Han <don_han@berkeley.edu>
2015-01-01 13:05:46 +0900

    finished basic timer

--- Pomodoro.py
+++ Pomodoro.py
@@ -1,20 +1,24 @@
 from datetime import datetime
 from time import sleep
 class Timer:
-    def __init__(self, interval, message):
-        self.interval = interval * 60 # minute -> seconds
-        self.message = message
-    def run(self):
-        remaining = self.interval
+    def run(self, interval, message=""):
+        remaining = interval * 60    # minute -> seconds
         startTime = datetime.now()
         print("Started at {0:02}:{1:02}'{2:02}".format(startTime.hour, startTime.minute, startTime.second))
-        for i in range(remaining)
-            mins, secs = secondsToTime(i)
-            print("{0:02}:{1:02} {2}".format(mins, secs, self.message), end="\r") 
+        for i in range(remaining):
+            mins, secs = self.secondsToTime(remaining - i)
+            print("{0:02}:{1:02} {2}".format(mins, secs, message), end="\r") 
             sleep(1)
         endTime = datetime.now()
-        print("Ended at {0:02}:{1:02}'{2:02}".format(endTime.hour, endTime.minute, endTime.second ))
-    def secondsToTime(s):
+        print("Ended at {0:02}:{1:02}'{2:02}".format(endTime.hour, endTime.minute, endTime.second))
+
+    def secondsToTime(self,s):
         minutes = s // 60
         seconds = s - (minutes * 60)
         return minutes, seconds
+
+if __name__ == "__main__":
+    #Timer().run(1)
+    #Timer().run(1,"hello")
+    pomo = Timer()
+    pomo.run(1)
