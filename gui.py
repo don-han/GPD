1d435f4a85f75fa7361f8673f9e491c4b6b7ec09
Don Han <don_han@berkeley.edu>
2015-01-04 10:31:42 +0900

    Can change MainLoop widget without needing to run a new loop

--- gui.py
+++ gui.py
@@ -3,21 +3,35 @@ import urwid
 def keyhandler(key):
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
-    # Add
+    # Collect
     if key in ('a', 'A'):
-        txt = urwid.Text(u"Hello world")
-        body = urwid.Filler(txt)
-        fr = urwid.Frame(body)
-        urwid.MainLoop(fr).run()
+
+        pass
+    # Process
+    if key in ('p', 'P'):
+        # make widget about processing (menu)
+        # loop.widget = 
+        pass
+
+    # do
+    if key in ('d', 'D'):
+        # make big text widget
+        # loop.widget=
+        pass
+
 
 class QuestionBox(urwid.Filler):
     def keypress(self, size, key):
         if key != 'enter':
             return super(QuestionBox, self).keypress(size, key)
+        self.original_widget=urwid.Text(u'The task "{0}" is added to the Collection Bucket'.format(footer.edit_text))
 
 if __name__ == "__main__":
-    txt = urwid.Text(u"Enter 'a' to add a task", align='center')
-    body = urwid.Filler(txt)
-    fr = urwid.Frame(body)
+    body_txt = urwid.Text(u"Enter 'a' to add a task", align='center')
+    body = urwid.Filler(body_txt)
+    footer= urwid.Edit(u"What task would you like to add?")
+    # header = prompt
+    #footer = urwid.Edit(u"What task would you like to add? ")
+    fr = urwid.Frame(body,footer=footer, focus_part='footer')
     loop = urwid.MainLoop(fr,unhandled_input=keyhandler)
     loop.run()
--- todo.md
+++ todo.md
@@ -1,9 +1,13 @@
 Written in the Order of Priority
+Version 1.0
 -/ Make Pomodoro Counter
 - Make TUI with urwid (-ncurses-)
     -/ draw on paper before you begin programming
-    - by referencing the manual, note the UI
+    -/by referencing the manual, note the UI
 - Write readme 
+
+Version 2.0
+- Xeffect into the system?
 
 DONE:
 
